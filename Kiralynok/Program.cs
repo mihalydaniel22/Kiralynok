6b8a72176fe637c409c31577e7e18336c41a88e2
mihalydaniel22 <rikkiardo3@gmail.com>
2020-09-16 08:29:04 +0200

    Add project files.

---
+++ Kiralynok/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiralynok
+{
+    class Table
+    {
+        char[,] T;
+        char UresCella;
+        int UresOszlopokSzama;
+        int UresSorokSzama;
+    }
+    class Program
+    {
+        static void Elhelyez()
+        {
+
+        }
+        static void Fajlbair()
+        {
+
+        }
+        static void Megjelenit()
+        {
+
+        }
+        static Tabla(char)
+        {
+
+        }
+        public int UresOszlop()
+        {
+            return 0;
+        }
+        public int UresSor()
+        {
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Elhelyez();
+            Fajlbair();
+            Megjelenit();
+            Tabla();
+            UresOszlop();
+            UresSor();
+
+        }
+    }
+}
